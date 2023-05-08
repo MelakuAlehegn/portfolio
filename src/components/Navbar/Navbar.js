1b77c9eccf491f0ae1f46edfb032ef60967ca9e6
unknown <melakualehegn34@gmail.com>
2023-05-08 23:26:09 +0300

    navbar css finished for desktop

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import logo from '../images/logo1.png'
+import "./Navbar.css"
 // import Button from '../Button/Button';
 import { Button } from '@material-ui/core'
 function Navbar() {
     return (
-        <div>
-            Navbar
-            {/* <Button name={'resume'} /> */}
-            <Button variant="outlined" color="primary">
-                Resume
-            </Button>
+        <div className='Navbar'>
+            <img src={logo} className='logo' />
+            <ul>
+                <li>about</li>
+                <li>skills</li>
+                <li>services</li>
+                <li>experience</li>
+                <li>projects</li>
+                <li>
+                    <button className='Resume'>Resume</button>
+                </li>
+            </ul>
         </div>
     )
 }
